628055261e621f6a778a01cc9dfd26e492d63383
nicole <105820202+nekokshg@users.noreply.github.com>
2025-03-03 08:23:05 -0800

    Implement navbar with styling

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,13 @@
 //Main component that sets up routing and contains layout elements like navigation
 import React, { useState, useEffect } from 'react';
+import './styles/App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import Home from './pages/Home';
 import Login from './pages/Login';
 import Register from './pages/Register';
 import Profile from './pages/Profile';
 import Search from './pages/Search';
+import Navbar from './components/Navbar';
 
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
@@ -23,25 +25,29 @@ const App = () => {
   }
 
   return (
-    <Router>
-      <Routes>
-        {/* Public Routes */}
-        <Route path='/' element={<Home isAuthenticated={isAuthenticated} logoutUser={logoutUser}/>} /> 
+      <Router>
+        <div className='appContainer'>
+          <Navbar />
+          <div className='mainContentContainer'>
+            <Routes>
+              {/* Public Routes */}
+              <Route path='/' element={<Home isAuthenticated={isAuthenticated} logoutUser={logoutUser}/>} /> 
 
-        {/* Only show login or register if the user is NOT authenticated */}
-        <Route path='/login' element={isAuthenticated ? <Navigate to='/profile' /> : <Login setIsAuthenticated={setIsAuthenticated}/>} />
-        <Route path='/register' element={isAuthenticated ? <Navigate to='/profile' /> : <Register setIsAuthenticated={setIsAuthenticated}/>} />
+              {/* Only show login or register if the user is NOT authenticated */}
+              <Route path='/login' element={isAuthenticated ? <Navigate to='/profile' /> : <Login setIsAuthenticated={setIsAuthenticated}/>} />
+              <Route path='/register' element={isAuthenticated ? <Navigate to='/profile' /> : <Register setIsAuthenticated={setIsAuthenticated}/>} />
 
-        <Route path ='/search' element={<Search />} />
+              <Route path ='/search' element={<Search />} />
 
-        {/* Private Routes */}
-        <Route 
-          path='/profile'
-          element={isAuthenticated ? <Profile/> : <Navigate to='/login' />}
-        />
-
-      </Routes>
-    </Router>
+              {/* Private Routes */}
+              <Route 
+                path='/profile'
+                element={isAuthenticated ? <Profile/> : <Navigate to='/login' />}
+              />
+            </Routes>
+          </div>
+        </div>
+      </Router>
   )
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,1 +1,40 @@
 //A component that contains links to different pages (e.g., Home, Search Books, User Profile, etc...)
+
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/Navbar.css';
+import appleBooks from '../assets/apple-books.svg';
+import home from '../assets/home.svg';
+import search from '../assets/search.svg';
+import profile from '../assets/user.svg';
+
+const Navbar = () => {
+    return (
+        <div className='navContainer'>
+            <ul className='navList'>
+                <li className='navItem'>
+                    <Link to='/' className='navLink'>
+                        <img src={appleBooks} alt='Logo' className='navImage'/>
+                    </Link>
+                </li>
+                <li className='navItem'>
+                    <Link to='/' className='navLink'>
+                        <img src={home} alt='Home' className='navImage'/>
+                    </Link>
+                </li>
+                <li className='navItem'>
+                    <Link to='/Search' className='navLink'>
+                        <img src={search} alt='Search' className='navImage'/>
+                    </Link>
+                </li>
+                <li className='navItem'>
+                    <Link to='/Profile' className='navLink'>
+                        <img src={profile} alt='Profile' className='navImage'/>
+                    </Link>
+                </li>
+            </ul>
+        </div>
+    )
+}
+
+export default Navbar;
